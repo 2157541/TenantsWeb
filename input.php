5431f80b599ce29c32a7908181f569f2761c5dc2
Wayne Tayaban <wayne+2157541@slu.edu.ph>
2019-12-11 21:48:06 +0800

    databse

--- input.php
+++ input.php
@@ -4,8 +4,45 @@
   <head>
   <title>Registration system PHP and MySQL</title>
   <link rel="stylesheet" type="text/css" href="style.css">
+  <style>
+  table, th, td {
+  	border: 1px solid black;
+  	border-collapse: collapse;
+  }
+
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+  }
+
+  li {
+    float: left;
+  }
+
+  li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+  }
+
+  li a:hover {
+    background-color: #111;
+  }
+
+  </style>
   </head>
 <body>
+  <ul>
+    <li><a class="active" href="index.html">Home</a></li>
+    <li><a href="input.php">Request Form</a></li>
+    <li><a href="status.php">Request Status</a></li>
+    <li><a href="payment.php">Payment</a></li>
+  </ul>
   <div class="header">
 		<h2>Register</h2>
 	</div>
